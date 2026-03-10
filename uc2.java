6e426657648a32246dd57900ef2ada961f3edef5
tejaphanimanikanta3102008-source <tejaphanimanikanta3102008@gmail.com>
2026-03-10 13:18:51 +0530

    Enhance output with new pattern display
    Added additional print statements to display a new pattern.

--- uc2.java
+++ uc2.java
@@ -2,13 +2,13 @@ public class uc2{
 
     public static void main(String[] args) {
 
-        System.out.println("  ***  " + "   ***   " + " ***** " + "   ***** ");
-        System.out.println(" *   * " + "  *   *  " + " *    * " + "  *     *");
-        System.out.println("*     *" + " *     * " + " *    * " + "  *      ");
-        System.out.println("*     *" + " *     * " + " *****  " + "   ***** ");
-        System.out.println("*     *" + " *     * " + " *      " + "        *");
-        System.out.println(" *   * " + "  *   *  " + " *      " + "  *     *");
-        System.out.println("  ***  " + "   ***   " + " *      " + "   ***** ");
+        System.out.println("  *****  " + "   *****   " + " *****   " + "   *****  ");
+        System.out.println(" **   ** " + "  **   **  " + " **   ** " + "  **      ");
+        System.out.println(" **   ** " + "  **   **  " + " **   ** " + "  **      ");
+        System.out.println(" **   ** " + "  **   **  " + " *****   " + "   *****  ");
+        System.out.println(" **   ** " + "  **   **  " + " **      " + "       ** ");
+        System.out.println(" **   ** " + "  **   **  " + " **      " + "       ** ");
+        System.out.println("  *****  " + "   *****   " + " **      " + "   *****  ");
 
     }
 }
